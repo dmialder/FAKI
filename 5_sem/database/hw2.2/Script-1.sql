30d00da5643067b1613a7588f5ce5dab630884dc
dmialder <55893537+dmialder@users.noreply.github.com>
2023-12-22 17:07:09 +0300

    Update Script-1.sql

--- 5_sem/database/hw2.2/Script-1.sql
+++ 5_sem/database/hw2.2/Script-1.sql
@@ -1,4 +1,3 @@
-select extract('year' from "dateId") as "year", extract('month' from "dateId") as "month", 
-"branchId", sum("salesRub") from distributor.sales s2  
+select "branchId", extract('year' from "dateId") as "year", extract('month' from "dateId") as "month", round(sum("salesRub")) as "total_salesRub" from distributor.sales s2  
 group by "year", "month", "branchId"
-order by "year", "month"
+order by "branchId", "year", "month"
