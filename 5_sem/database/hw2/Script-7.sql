fc7fbc7fe58fde7dcd4960a4ea2974a0a3e0e676
dmialder <55893537+dmialder@users.noreply.github.com>
2023-12-22 16:12:48 +0300

    Update Script-7.sql

--- 5_sem/database/hw2/Script-7.sql
+++ 5_sem/database/hw2/Script-7.sql
@@ -1,5 +1,2 @@
-
-
-
 select extract('year' from "dateId") as "year", extract('isodow' from "dateId") as "day_of_week", sum("salesRub") from distributor.singlesales s 
 group by "year", "day_of_week"
